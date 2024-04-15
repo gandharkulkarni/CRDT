679f320bbdcbb3de0fe4d5f0fbf0b435020fca03
gandharkulkarni <gakulkarni@dons.usfca.edu>
2024-04-14 17:03:53 -0700

    Refactored constructor

--- src/last_write_wins/lww_register.go
+++ src/last_write_wins/lww_register.go
@@ -2,21 +2,21 @@ package lww
 
 /* Properties are not exported */
 type State struct {
-	Peer      string
-	Timestamp int64
-	Value     string
+	peer      string
+	timestamp int64
+	value     string
 }
 type LWWRegister struct {
-	Id    string
-	State State
+	id    string
+	state State
 }
 
 // ** Private methods **//
 func (local *LWWRegister) setState(value string) {
-	local.State = State{
-		Peer:      local.Id,
-		Timestamp: local.State.Timestamp + 1,
-		Value:     value,
+	local.state = State{
+		peer:      local.id,
+		timestamp: local.state.timestamp + 1,
+		value:     value,
 	}
 	// local.state.Timestamp += 1
 	// local.state.Value = value
@@ -24,23 +24,28 @@ func (local *LWWRegister) setState(value string) {
 
 // ** Public methods **//
 func (local *LWWRegister) GetValue() string {
-	return local.State.Value
+	return local.state.value
 }
-func InitializeLWWRegister(id string, state State) *LWWRegister {
+
+func InitializeLWWRegister(id string, peer string, timestamp int, value string) *LWWRegister {
 	return &LWWRegister{
-		Id:    id,
-		State: state,
+		id: id,
+		state: State{
+			peer:      peer,
+			timestamp: int64(timestamp),
+			value:     value,
+		},
 	}
 }
 func (local *LWWRegister) Merge(state State) {
-	if local.State.Timestamp > state.Timestamp {
+	if local.state.timestamp > state.timestamp {
 		return
 	}
 
-	if local.State.Timestamp == state.Timestamp && local.State.Peer > state.Peer {
+	if local.state.timestamp == state.timestamp && local.state.peer > state.peer {
 		return
 	}
 
-	local.State = state
+	local.state = state
 
 }
