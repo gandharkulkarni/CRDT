49b5bf88d62a28fe56332b2241f17d3758b16369
gandharkulkarni <gakulkarni@dons.usfca.edu>
2024-04-14 17:04:03 -0700

    Minor changes

--- src/collab_node/collab_node.go
+++ src/collab_node/collab_node.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"crdt/src/comms_handler"
+	"crdt/src/constants"
 	"crdt/src/helper"
+	lww "crdt/src/last_write_wins"
 	"flag"
 	"fmt"
 	"net"
@@ -18,8 +21,8 @@ type collabNode struct {
 }
 
 func main() {
-	port := flag.Int("port", 7000, "Listner port")
-	central := flag.String("central", "", "Central machine:port")
+	port := flag.Int("port", constants.COLLAB_PORT, "Listner port")
+	central := flag.String("central", constants.CENTRAL, "Central machine:port")
 	collaborator := flag.String("collab", "", "Other collaborator node name")
 
 	flag.Parse()
@@ -43,11 +46,12 @@ func main() {
 	// If machine name not given, register with central node port connect to
 	registerWithCentralNode(centralHostDetails, machine, int64(*port))
 	if *collaborator != "" {
-		// TODO: If machine name given, register with central node and get port no for machine name, connect to port+1
-		getCollabNodeDetails(queryHostDetails, *collaborator)
+		//? If machine name given, register with central node and get port no for machine name, connect to port+1
+		collabSource := getCollabNodeDetails(queryHostDetails, *collaborator)
+	} else {
+		// Start CRDT environment
+		startCollabEnvironment(machine)
 	}
-	// TODO: Start CRDT environment
-	startCollabEnvironment()
 }
 func connectToCentralNode(centralHostDetails string) net.Conn {
 	conn, err := net.Dial("tcp", centralHostDetails)
@@ -90,7 +94,18 @@ func getCollabNodeDetails(centralHostDetails string, collaborator string) collab
 	}
 }
 
-func startCollabEnvironment() {
+func startCollabEnvironment(machine string) {
+	var lwwReg *lww.LWWRegister = lww.InitializeLWWRegister(machine, machine, 0, "")
+	scanner := bufio.NewScanner(os.Stdin)
 	for {
+		input := scanner.Text()
+
+		//TODO: Need to write a method to change the state text
+
+		//TODO: listen for updates from collab node
+		//TODO: Call merge function
+
+		//TODO: May have to change proto for collab msg
+
 	}
 }
